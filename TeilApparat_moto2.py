b35469d3dc7ba2588914bab9046837ff05c8713a
Ing-Josef-Klotzer <josef.klotzner@gmail.com>
2019-02-25 16:08:07 +0100

    Teilapp added Pause with float

--- TeilApparat_moto2.py
+++ TeilApparat_moto2.py
@@ -21,7 +21,7 @@ def main ():
     # schreibe hier Dein Hauptprogramm
 
     Teilungen = int (input ("Bitte geben Sie die Anzahl der gewünschten Teilungen ein: "))
-    Pause = int(input ("Bitte geben Sie die Pause (s) zwischen Teilschritten ein: "))
+    Pause = float(input ("Bitte geben Sie die Pause (s) zwischen Teilschritten ein: "))
     #Teilungen = 12
     #Pause = 1
 #    for i in range (1, 21):
@@ -29,8 +29,8 @@ def main ():
 #        print ("Teilungsliste", liste, "Summe", sum (liste))
     liste = Teilungsliste (Teilungen)
     print (liste)
-
-    vorStepM2 (liste, Pause)
+    for i in range (30):
+        vorStepM2 (liste, Pause)
 
 #    for i, steps in zip (range (1, Teilungen + 1), liste):
 #        print ("Bewege Motor im Segment", i, "um", steps, "Schritte")
